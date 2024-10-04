c6f126a7037b957b8cf30fa4dbc224b3df00c838
wendellxavier <wendellx25@gmail.com>
2024-10-04 16:51:54 -0300

    adicionando menssagens

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def cadastro(request):
     if request.method == "GET":
@@ -12,14 +13,17 @@ def cadastro(request):
 
 
     if senha != confirmar_senha:
+        messages.add_message(request, constants.ERROR, 'Senha e confirmar senha estão diferentes')
         return redirect('/usuarios/cadastro/')
     
     if len(senha) < 5:
+        messages.add_message(request, constants.WARNING, 'Senha precisa ter mais de 4 caracteres')
         return redirect('/usuarios/cadastro/')
     
     users = User.objects.filter(username=username)
 
     if users.exists:
+        messages.add_message(request, constants.ERROR, 'Já existe um usuario com esse username')
         return redirect('/usuarios/cadastro')
     
     user = User.objects.create_user(username=username, password=senha)
